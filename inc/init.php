845ddccb3b996b1aec6c34cc31c2dc9f73563e57
Matthew Lutz <me@mlutz.us>
2017-11-25 20:12:23 -0600

    Create init.php

---
+++ inc/init.php
@@ -0,0 +1,12 @@
+<?php
+  class Display {
+    function output() {
+      $html = "";
+      exit($html);
+    }
+  }
+  class Utility {
+  }
+  $display = new Display;
+  $utility = new Utility;
+?>
